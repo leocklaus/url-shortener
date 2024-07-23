23d15999dcfa27d30cdff80679576261db804034
leocklaus <leocklaus@gmail.com>
2024-07-23 15:27:25 -0300

    feat: validations added

--- src/main/java/io/github/leocklaus/url_sortener/api/dto/URLInputDTO.java
+++ src/main/java/io/github/leocklaus/url_sortener/api/dto/URLInputDTO.java
@@ -1,6 +1,8 @@
 package io.github.leocklaus.url_sortener.api.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record URLInputDTO(UUID userId, String originalURL) {
+public record URLInputDTO(@NotNull UUID userId, @NotNull String originalURL) {
 }
--- src/main/java/io/github/leocklaus/url_sortener/api/dto/UserInputDTO.java
+++ src/main/java/io/github/leocklaus/url_sortener/api/dto/UserInputDTO.java
@@ -1,4 +1,11 @@
 package io.github.leocklaus.url_sortener.api.dto;
 
-public record UserInputDTO(String name, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UserInputDTO(
+        @NotNull @Size(min = 3) String name,
+        @NotNull @Email String email,
+        @NotNull @Size(min = 6) String password) {
 }
