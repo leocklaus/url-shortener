910d09f8b57047031e23ad24846d6c7c326b9528
leocklaus <leocklaus@gmail.com>
2024-07-23 10:01:09 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/url_sortener/UrlSortenerApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.url_sortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlSortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlSortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/url_sortener/UrlSortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.url_sortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlSortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
